cd525e6f813601a694dc12ac43c233d1a089a85c
Nick <nick@gmx.co.uk>
2023-09-21 22:00:20 +0300

    Schedule poweroff

--- cloud-init.sh
+++ cloud-init.sh
@@ -3,6 +3,11 @@
 set -eux
 trap 'poweroff' TERM EXIT INT
 
+# poweroff script
+cat >> /etc/crontab << 'EOF'
+*/1 * * * * root poweroff +5
+EOF
+
 # basics
 apt-get update
 apt-get install -y curl jq git ca-certificates gnupg lsb-release
