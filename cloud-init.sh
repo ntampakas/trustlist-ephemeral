81d1f977d8a89ba34f1ad98b4c62d6cd142eb1e9
Nick <nick@gmx.co.uk>
2023-09-21 22:38:52 +0300

    Remove cron

--- cloud-init.sh
+++ cloud-init.sh
@@ -3,13 +3,8 @@
 set -eux
 trap 'poweroff' TERM EXIT INT
 
-# poweroff script
-cat >> /etc/crontab << 'EOF'
-*/1 * * * * root sleep 1
-EOF
-
 # basics
 apt-get update
 apt-get install -y curl jq git ca-certificates gnupg lsb-release
 
-sleep 600
+sleep 120
