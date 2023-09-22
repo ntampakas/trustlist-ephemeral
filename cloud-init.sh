4c606906802d23e1ea914bf6a8aeecc7de9f4a79
Nick <nick@gmx.co.uk>
2023-09-22 10:37:18 +0300

    Install pkgs

--- cloud-init.sh
+++ cloud-init.sh
@@ -3,8 +3,19 @@
 set -eux
 trap 'poweroff' TERM EXIT INT
 
-# basics
+# Install pkgs
 apt-get update
-#apt-get install -y curl jq git ca-certificates gnupg lsb-release
+apt-get install -y net-tools
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+usermod -aG docker ubuntu
 
 sleep 12h
